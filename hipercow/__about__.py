f1ed3f97a547187777112a092b8cbbdeda279e4d
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-01-02 11:01:51 +0000

    Update structure closer to outpack

--- hipercow/__about__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2024-present Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
-#
-# SPDX-License-Identifier: MIT
-__version__ = '0.0.1'
--- hipercow/__init__.py
+++
@@ -1,3 +0,0 @@
-# SPDX-FileCopyrightText: 2024-present Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
-#
-# SPDX-License-Identifier: MIT
