b639ac6983094376c03f4e4b3ad500d7b0be0c25
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2025-01-27 11:10:51 +0000

    Add basic task commands

--- src/hipercow/cli/__init__.py
+++ src/hipercow/cli/__init__.py
@@ -1,5 +1,9 @@
 import click
 
+from hipercow.root import open_root
+from hipercow.task import task_status
+from hipercow.task_create import task_create
+
 
 @click.group()
 def cli():
@@ -10,12 +14,23 @@ def cli():
 def init(path: str):
     click.echo(f"Initialise hipercow at '{path}'")
 
+
 @cli.group()
 def task():
     pass
 
+
 @task.command()
 @click.argument("task_id", type=str)
-@click.option("-r", "--root", help="Hipercow root directory", type=click.Path())
-def status(task_id: str, root=None):
-    click.echo(f"Getting task status for '{task_id}'")
+def status(task_id: str):
+    root = open_root()
+    click.echo(task_status(root, task_id))
+
+
+@task.command()
+@click.argument("cmd", nargs=-1)
+def create(cmd: str):
+    root = open_root()
+    envvars = {}
+    task_id = task_create(root, "shell", list(cmd), envvars)
+    click.echo(task_id)
