f75a4b4ba031486adeef56a5036c59fd0a0b90bd
Wes Hinsley <w.hinsley@imperial.ac.uk>
2025-02-01 15:04:47 +0000

    Try again

--- src/hipercow/dide/mounts.py
+++ src/hipercow/dide/mounts.py
@@ -54,7 +54,7 @@ def detect_mounts() -> list[Mount]:
 def _detect_mounts_unix() -> list[Mount]:
     try:
         sysname = platform.system()
-    except Exception as ex:
+    except Exception:
         sysname = "Windows"
     fstype = _unix_smb_mount_type(sysname)
     res = subprocess.run(
