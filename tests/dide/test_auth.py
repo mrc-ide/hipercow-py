2a8c367015e2f87749d3bf553dac9633d4679540
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2025-02-05 12:39:16 +0000

    Start on tests

---
+++ tests/dide/test_auth.py
@@ -0,0 +1,33 @@
+import pytest
+from unittest import mock
+
+from hipercow.dide import auth
+
+
+def test_strip_leading_dide_from_username():
+    assert auth._check_username("DIDE\\bob") == "bob"
+    assert auth._check_username("dide\\bob") == "bob"
+    assert auth._check_username("bob") == "bob"
+    assert auth._check_username("other\\bob") == "other\\bob"
+
+
+def test_error_for_invalid_usernames():
+    with pytest.raises(Exception, match="empty username"):
+        auth._check_username("")
+    with pytest.raises(Exception, match="Unexpected newline"):
+        auth._check_username("foo\n bar")
+    with pytest.raises(Exception, match="Unexpected '#'"):
+        auth._check_username("username # your username here")
+    with pytest.raises(Exception, match="Unexpected ' '"):
+        auth._check_username("username here")
+
+
+def test_can_get_password(mocker):
+    mocker.patch("getpass.getpass", return_value="bob")
+    assert auth._get_password() == "bob"
+
+
+def test_can_error_if_given_empty_password(mocker):
+    mocker.patch("getpass.getpass", return_value="")
+    with pytest.raises(Exception, match="Invalid empty password"):
+        assert auth._get_password()
