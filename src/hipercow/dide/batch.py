6a101254fbfcbcf424bc88ac78846ce3385a8be5
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2025-03-03 10:06:42 +0000

    Add dide support

--- src/hipercow/dide/batch.py
+++ src/hipercow/dide/batch.py
@@ -7,14 +7,14 @@ from hipercow.dide.mounts import PathMap
 from hipercow.root import Root
 
 TASK_RUN = Template(
-    """@echo off
+    r"""@echo off
 REM automatically generated
 ECHO generated on host: ${hostname}
 ECHO generated on date: ${date}
 ECHO hipercow(py) version: ${hipercow_version}
 ECHO running on: %COMPUTERNAME%
 
-net use I: \\\\wpia-hn\\hipercow /y
+net use I: \\wpia-hn\hipercow /y
 
 ${network_shares_create}
 
@@ -28,19 +28,19 @@ set REDIS_URL=10.0.2.254
 
 ECHO this is a single task
 
-I:\\bootstrap-py\\python-311\\bin\\hipercow task eval --capture ${task_id}
+I:\bootstrap-py\python-311\bin\hipercow task eval --capture ${task_id}
 
 @ECHO off
-set ErrorCodeTask=%ERRORLEVEL%
+set ErrorCode=%ERRORLEVEL%
 
 @REM We could use hipercow here, I think
-if exist hipercow\\tasks\\${task_id_1}\\${task_id_2}\\status-success (
+if exist hipercow\tasks\${task_id_1}\${task_id_2}\status-success (
   set TaskStatus=0
 ) else (
   set TaskStatus=1
 )
 
-ECHO ERRORLEVEL was %ErrorCodeTask%
+ECHO ERRORLEVEL was %ErrorCode%
 
 ECHO Cleaning up
 %SystemDrive%
@@ -49,9 +49,9 @@ ${network_shares_delete}
 
 net use I: /delete /y
 
-if %ErrorCodeTask% neq 0 (
+if %ErrorCode% neq 0 (
   ECHO Task failed catastrophically
-  EXIT /b %ErrorCodeTask%
+  EXIT /b %ErrorCode%
 )
 
 if %TaskStatus% == 0 (
@@ -61,6 +61,50 @@ if %TaskStatus% == 0 (
   ECHO Task did not complete successfully
   EXIT /b 1
 )"""
+)
+
+PROVISION = Template(
+    r"""@echo off
+REM automatically generated
+ECHO generated on host: ${hostname}
+ECHO generated on date: ${date}
+ECHO hipercow(py) version: ${hipercow_version}
+ECHO running on: %COMPUTERNAME%
+
+net use I: \\wpia-hn\hipercow /y
+
+${network_shares_create}
+
+${hipercow_root_drive}
+cd ${hipercow_root_path}
+ECHO working directory: %CD%
+
+ECHO this is a provisioning task
+
+I:\bootstrap-py\python-311\bin\hipercow environment build --name ${environment_name} ${provision_id}
+
+@ECHO off
+%SystemDrive%
+set ErrorCode=%ERRORLEVEL%
+
+ECHO ERRORLEVEL was %ErrorCode%
+
+ECHO Cleaning up
+%SystemDrive%
+
+${network_shares_delete}
+
+net use I: /delete /y
+
+set ERRORLEVEL=%ErrorCode%
+
+if %ERRORLEVEL% neq 0 (
+  ECHO Error running provisioning
+  EXIT /b %ERRORLEVEL%
+)
+
+@ECHO Quitting
+"""  # noqa: E501
 )
 
 
@@ -84,7 +128,25 @@ def write_batch_task_run(task_id: str, path_map: PathMap, root: Root) -> str:
     return unc
 
 
-def _template_data_task_run(task_id, path_map: PathMap) -> dict:
+def write_batch_provision(
+    name: str, provision_id: str, path_map: PathMap, root: Root
+) -> str:
+    data = _template_data_provision(name, provision_id, path_map)
+    path = str(
+        root.path_provision(name, provision_id, relative=True) / "run.bat"
+    )
+    path = path.replace("\\", "/")
+    rel = path_map.relative
+    unc = f"//{path_map.mount.host}/{path_map.mount.remote}/{rel}/{path}"
+
+    path_abs = root.path / path
+    path_abs.parent.mkdir(parents=True, exist_ok=True)
+    with (path_abs).open("w") as f:
+        f.write(PROVISION.substitute(data))
+    return unc
+
+
+def _template_data_core(path_map: PathMap) -> dict[str, str]:
     host = path_map.mount.host
     unc_path = f"//{host}/{path_map.mount.remote}".replace("/", "\\")
     root_drive = path_map.remote
@@ -97,11 +159,25 @@ def _template_data_task_run(task_id, path_map: PathMap) -> dict:
         "hostname": platform.node(),
         "date": str(datetime.datetime.now(tz=datetime.timezone.utc)),
         "hipercow_version": version,
-        "task_id": task_id,
-        "task_id_1": task_id[:2],
-        "task_id_2": task_id[2:],
         "hipercow_root_drive": root_drive,
         "hipercow_root_path": root_path,
         "network_shares_create": network_shares_create,
         "network_shares_delete": network_shares_delete,
+    }
+
+
+def _template_data_task_run(task_id, path_map: PathMap) -> dict[str, str]:
+    return _template_data_core(path_map) | {
+        "task_id": task_id,
+        "task_id_1": task_id[:2],
+        "task_id_2": task_id[2:],
+    }
+
+
+def _template_data_provision(
+    name: str, id: str, path_map: PathMap
+) -> dict[str, str]:
+    return _template_data_core(path_map) | {
+        "environment_name": name,
+        "provision_id": id,
     }
--- src/hipercow/dide/driver.py
+++ src/hipercow/dide/driver.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
 
+import taskwait
+
 from hipercow.dide.auth import fetch_credentials
-from hipercow.dide.batch import write_batch_task_run
+from hipercow.dide.batch import write_batch_provision, write_batch_task_run
 from hipercow.dide.mounts import PathMap, detect_mounts, remap_path
 from hipercow.dide.web import DideWebClient
 from hipercow.driver import HipercowDriver
@@ -35,4 +37,45 @@ class DideDriver(HipercowDriver):
         cl.submit(unc, task_id)
 
     def provision(self, root: Root, name: str, id: str) -> None:
-        raise NotImplementedError()  # pragma: no cover
+        credentials = fetch_credentials()
+        cl = DideWebClient(credentials)
+        cl.login()
+        unc = write_batch_provision(name, id, self.config.path_map, root)
+        dide_id = cl.submit(unc, f"{name}/{id}")
+        ProvisionWaitWrapper(root, name, id, cl, dide_id)
+        # Once we have support, we need to block here and stream logs,
+        # but that erquires getting the logwatch functionality ported
+        # over.
+        print("A provisioning task has been submitted")
+        print("Have a look at the web portal to keep track on progress")
+
+
+class ProvisionWaitWrapper(taskwait.Task):
+    def __init__(
+        self,
+        root: Root,
+        name: str,
+        provision_id: str,
+        client: DideWebClient,
+        dide_id: str,
+    ):
+        self.root = root
+        self.name = name
+        self.provision_id = provision_id
+        self.client = client
+        self.dide_id = dide_id
+        self.status_waiting = {"created", "submitted"}
+        self.status_running = {"running"}
+
+    def status(self) -> str:
+        return str(self.client.status_job(self.dide_id))
+
+    def log(self) -> list[str] | None:
+        path = self.root.path_provision_log(self.name, self.provision_id)
+        if not path.exists():
+            return None
+        with path.open() as f:
+            return f.readlines()
+
+    def has_log(self) -> bool:
+        return True
