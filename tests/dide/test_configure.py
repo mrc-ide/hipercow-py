dc3d599c19e16979eaa3003e86f72f9243c81c88
Wes Hinsley <w.hinsley@imperial.ac.uk>
2025-07-04 16:47:12 +0100

    Mock web client arg

--- tests/dide/test_configure.py
+++ tests/dide/test_configure.py
@@ -107,6 +107,7 @@ def test_provision_using_driver(tmp_path, mocker):
     mock_mounts = [Mount(host="projects", remote="other", local=tmp_path)]
     mock_provision = mock.MagicMock()
     mocker.patch("hipercow.dide.driver.detect_mounts", return_value=mock_mounts)
+    mocker.patch("hipercow.dide.driver._web_client", return_value=mock.Mock())
     mocker.patch("hipercow.dide.driver._dide_provision_win", mock_provision)
     configure(
         "dide-windows", python_version=None, check_credentials=False, root=r
